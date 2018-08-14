92315f570fc5db18ac78da33cfe784b0a9c2826d
dabdevelop <develop@dab.vc>
2018-08-15 01:54:53 +0800

    refine the deploy scripts

--- compile_all.sh
+++ compile_all.sh
@@ -6,23 +6,25 @@ if [ -d "build" ]; then
 	rm -rf build
 fi
 
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/EasyDABFormula.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/DepositToken.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/CreditToken.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/SubCreditToken.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/DiscreditToken.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/DepositTokenController.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/CreditTokenController.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/SubCreditTokenController.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/DiscreditTokenController.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/DABDepositAgent.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/DABCreditAgent.sol
+SOURCE_DIR=solidity/contracts
 
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/DAB.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/DABWalletFactory.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/EasyDABFormula.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/DepositToken.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/CreditToken.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/SubCreditToken.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/DiscreditToken.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/DepositTokenController.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/CreditTokenController.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/SubCreditTokenController.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/DiscreditTokenController.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/DABDepositAgent.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/DABCreditAgent.sol
 
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/AMonthLoanPlanFormula.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/AYearLoanPlanFormula.sol
-solc ..=.. --optimize --bin --abi --hashes --allow-paths solidity/contracts -o build --overwrite solidity/contracts/TwoYearLoanPlanFormula.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/DAB.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/DABWalletFactory.sol
+
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/AMonthLoanPlanFormula.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/AYearLoanPlanFormula.sol
+solc ..=.. --optimize --bin --abi --hashes --allow-paths $SOURCE_DIR -o build --overwrite $SOURCE_DIR/TwoYearLoanPlanFormula.sol
 
 
